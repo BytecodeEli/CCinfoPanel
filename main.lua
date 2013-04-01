19072581947b76cc9b349d759fdb1f31afd921b3
D3add3d <d3add3d@gmail.com>
2013-04-01 17:37:34 +0300

    Updated to work with master API and globals

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
-os.loadAPI("master")
-master.heading("CC INFO PANEL")
-master.fillTable()
+if os.loadAPI("master") == true then
+  print("API loaded!")
+elseif os.loadAPI("master") != true and fs.exists("master") then
+  print("API already loaded!")
+elseif os.loadAPI("master") != true and fs.exists("master") != true then
+  print("API not found!")
+end
+master.heading(loadedconf[3])
+master.setTable("Refresh", refresh, 35, 50, 17, 20)
 while true do
+mon.clear()
 master.screen()
-master.heading("CC INFO PANEL")
+master.heading(loadedconf[3])
 master.info()
-master.screen()
 local e,side,x,y = os.pullEvent("monitor_touch")
 master.checkxy(x,y)
 sleep(.1)
